546f1de0559bc48cd39117026fb44eb338adc2f9
Feng Mu <fengmu@Fengs-MacBook-Pro.local>
2019-07-20 23:16:35 -0700

    redux almost ready

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import reducer from "./redux/reducer";
 
-function App() {
+const store = createStore(reducer, applyMiddleware(thunk));
+
+const App = () => {
   return (
-    <div className="App">
-      hello world
-    </div>
+    <Provider store={store}>
+      <div>hello world</div>
+    </Provider>
   );
-}
+};
 
 export default App;
---
+++ src/redux/reducer/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+
+const reducers = combineReducers({
+  // ... your other reducers here ...
+  // form: formReducer
+});
+
+export default reducers;
