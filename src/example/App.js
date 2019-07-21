ed7fea9db86050ad8b35170bc1fc27ffb4686e52
Feng Mu <fengmu@Fengs-MacBook-Pro.local>
2019-07-20 23:28:25 -0700

    redux and redux-form is ready

--- src/example/App.js
+++ src/example/App.js
@@ -3,13 +3,14 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import reducer from "./redux/reducer";
+import CustomTable from "../libs/CustomTable"
 
 const store = createStore(reducer, applyMiddleware(thunk));
 
 const App = () => {
   return (
     <Provider store={store}>
-      <div>hello world</div>
+      <CustomTable />
     </Provider>
   );
 };
--- src/example/redux/reducer/index.js
+++ src/example/redux/reducer/index.js
@@ -1,8 +1,9 @@
 import { combineReducers } from "redux";
+import { reducer as formReducer } from 'redux-form'
 
 const reducers = combineReducers({
   // ... your other reducers here ...
-  // form: formReducer
+  form: formReducer
 });
 
 export default reducers;
